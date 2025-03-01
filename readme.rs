1fca2a3a36dddfb98aab16fe7de4d782183b8994
Source Graph A.I. <117184625+UniInc@users.noreply.github.com>
2025-03-01 13:33:04 -0600

    Update readme.rs

--- readme.rs
+++ readme.rs
@@ -6,7 +6,7 @@ let market.search = [´https://www.google.com/search?q=VEVOR+Ratchet+Pipe+Thread
 let market = [
   ´https://www.harborfreight.com/12-in-1-in-ratcheting-pipe-threader-set-5-piece-57257.html´,
   ´https://www.homedepot.com/p/VEVOR-Ratchet-Pipe-Threader-Kit-1-2-in-NPT-3-4-in-NPT-1-in-NPT-Manual-Ratcheting-Pipe-Threader-Set-with-NPT-Dies-3-Piece-SDGLWQJ312341FZW2V0-0729/331970817´,
-  ]
+  ];
 
 let Review = [
   ('I Wish I Bought This Sooner! | VEVOR Ratchet Pipe Threader Kit','https://youtu.be/xThuDONv_gY')
