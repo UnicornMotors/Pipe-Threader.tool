cf785c7f7df546ac58bd32b8495b5cc0118425ec
Source Graph A.I. <117184625+UniInc@users.noreply.github.com>
2025-03-01 14:22:58 -0600

    Update readme.rs

--- readme.rs
+++ readme.rs
@@ -1,3 +1,6 @@
+/* Evoked! */
+let Evoked = 'https://www.vevor.com/manual-ratchet-threader-c_10847/vevor-ratchet-pipe-threader-kit-1-4-npt-1-1-4-npt-manual-ratcheting-pipe-threader-portable-pipe-threading-tool-set-with-6pcs-npt-dies-storage-case-for-galvanized-aluminum-iron-copper-pipes-p_010276098676';
+
 // Pipe-Threader.tool
 let search = ['https://www.google.com/search?q=pipe+threader'];
 
