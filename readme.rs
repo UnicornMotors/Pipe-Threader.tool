ecaf4620ea3bfb06ae4011c5d16fb69a1d47963a
Source Graph A.I. <117184625+UniInc@users.noreply.github.com>
2025-03-01 14:22:38 -0600

    Update readme.rs

--- readme.rs
+++ readme.rs
@@ -6,6 +6,7 @@ let market.search = [´https://www.google.com/search?q=VEVOR+Ratchet+Pipe+Thread
 let market = [
   ´https://www.harborfreight.com/12-in-1-in-ratcheting-pipe-threader-set-5-piece-57257.html´,
   ´https://www.homedepot.com/p/VEVOR-Ratchet-Pipe-Threader-Kit-1-2-in-NPT-3-4-in-NPT-1-in-NPT-Manual-Ratcheting-Pipe-Threader-Set-with-NPT-Dies-3-Piece-SDGLWQJ312341FZW2V0-0729/331970817´,
+  'https://www.vevor.com/manual-ratchet-threader-c_10847/vevor-ratchet-pipe-threader-kit-1-4-npt-1-1-4-npt-manual-ratcheting-pipe-threader-portable-pipe-threading-tool-set-with-6pcs-npt-dies-storage-case-for-galvanized-aluminum-iron-copper-pipes-p_010276098676',
   ];
 
 let Review = [
